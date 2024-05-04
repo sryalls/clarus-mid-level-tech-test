33eaa4570040ec6ec9ef4049ea2a8bdd692fcb93
SandyRyalls <sandy.ryalls@googlemail.com>
2024-05-04 12:04:26 +0100

    stock intake endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :stocks, only: [:index, :show]
+
+put '/stocks/:warehouse_id/:product_id/:quantity', to: 'stocks#intake'
   resources :warehouses
   resources :products
 end
